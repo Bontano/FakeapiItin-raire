0d74ae8326bc68ff70050186c5203228b906325b
Bontano <hassen.tom07@gmail.com>
2024-02-03 09:48:05 +0000

    saveAPI

--- app.js
+++ app.js
@@ -3,16 +3,36 @@ const app = express();
 const itineraires = require("./data.json");
 
 var cors = require("cors");
+app.use(express.json())
 app.use(cors());
 // Route pour obtenir les itinéraires d'une ville spécifique
-app.get("/itineraire/:ville", (req, res) => {
-  const ville = req.params.ville;
-  if (itineraires[ville]) {
-    res.json(itineraires[ville]);
-  } else {
-    res.status(404).send("Ville non trouvée");
+app.post("/itineraire", (req, res) => {
+  let content = req.body.prompt
+  let cities = [];
+  for (const key in itineraires) {
+    cities.push(key)
   }
+  let wordsArray = content.split(" ")
+  wordsArray.forEach(word => {
+    if (word !="") {
+      word = word.toLowerCase()
+      console.log(word)
+    if (cities.includes(word)) {
+      if (itineraires[word]) {
+        res.json(itineraires[word]);
+      } else {
+        res.status(404).send("Ville non trouvée");
+      }
+    }
+    }
+    
+  }); 
 });
+
+app.get("/villes", (req, res) => {
+    res.json(itineraires);
+});
+
 
 app.get("/test", (req, res) => {
   res.send("test");
