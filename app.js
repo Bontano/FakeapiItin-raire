f25d6f403826d0cbff4fa93b24c42e4c19f73a30
Bontano <hassen.tom07@gmail.com>
2024-02-08 14:53:46 +0000

    a

--- app.js
+++ app.js
@@ -3,36 +3,34 @@ const app = express();
 const itineraires = require("./data.json");
 
 var cors = require("cors");
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 // Route pour obtenir les itinéraires d'une ville spécifique
 app.post("/itineraire", (req, res) => {
-  let content = req.body.prompt
+  let content = req.body.prompt;
   let cities = [];
   for (const key in itineraires) {
-    cities.push(key)
+    cities.push(key);
   }
-  let wordsArray = content.split(" ")
-  wordsArray.forEach(word => {
-    if (word !="") {
-      word = word.toLowerCase()
-      console.log(word)
-    if (cities.includes(word)) {
-      if (itineraires[word]) {
-        res.json(itineraires[word]);
-      } else {
-        res.status(404).send("Ville non trouvée");
+  let wordsArray = content.split(" ");
+  wordsArray.forEach((word) => {
+    if (word != "") {
+      word = word.toLowerCase();
+      console.log(word);
+      if (cities.includes(word)) {
+        if (itineraires[word]) {
+          res.json({ [word]: itineraires[word] });
+        } else {
+          res.status(404).send("Ville non trouvée");
+        }
       }
     }
-    }
-    
-  }); 
+  });
 });
 
 app.get("/villes", (req, res) => {
-    res.json(itineraires);
+  res.json(itineraires);
 });
-
 
 app.get("/test", (req, res) => {
   res.send("test");
