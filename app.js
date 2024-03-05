a4027d1eeabd29b8c905cbb4c8426b78479a61c7
Bontano <hassen.tom07@gmail.com>
2024-03-05 14:46:49 +0000

    refactor

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const itineraires = require("./data.json");
 
-var cors = require("cors");
+const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 // Route pour obtenir les itinéraires d'une ville spécifique
